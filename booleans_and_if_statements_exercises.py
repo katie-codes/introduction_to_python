847e5fbbe41ef653d6e5f3db57832e5638cfff81
katie-codes <68138848+katie-codes@users.noreply.github.com>
2020-07-24 10:31:58 +1000

    Add files via upload

---
+++ booleans_and_if_statements_exercises.py
@@ -0,0 +1,65 @@
+
+
+#q1
+# moths_in_house = True
+
+# if moths_in_house:
+#     print ("Get the moths!")
+# else: 
+#     print("No threats detected.")
+
+
+#q2
+
+# moths_in_house = False
+# mitch_is_home = True
+
+# if moths_in_house and mitch_is_home:
+#     print ("Hoooman! Help me get the moths!")
+# else: 
+#     print ("No threats detected.")
+
+# if moths_in_house and not mitch_is_home:
+#     print ("Meooooooooooooow! Hissssss!")
+
+# if not moths_in_house and mitch_is_home:
+#     print ("Climb on Mitch")
+
+
+#q3
+
+# light_colour = "Amber"
+# car_detected = True
+
+# if light_colour == "Red" and car_detected == False:
+#     print ("Do Nothing")
+
+# if light_colour == "Red" and car_detected == True:
+#     print ("Flash")
+
+# if light_colour == "Green" and car_detected == False:
+#     print ("Do Nothing")
+
+# if light_colour == "Green" and car_detected == True:
+#     print ("Do Nothing")
+
+# if light_colour == "Amber" and car_detected == False:
+#     print ("Do Nothing")
+
+# if light_colour == "Amber" and car_detected == True:
+#     print ("Do Nothing")
+
+
+
+#q4 
+
+find_height = input("What is your height")
+height = int(find_height)
+
+if height <= 119:
+    print("Sorry, not today")
+else:
+    print("Hop on")
+
+
+
---
+++ variables_and_user_input_exercises.py
@@ -0,0 +1,42 @@
+
+#q1
+# cats = input("how many cats do you have?")
+# dogs = input("how many dogs do you have?")
+# total = int(cats)+float(dogs)
+# print(f"Wow, all together you have {total} cats and dogs")
+
+#changed the second input to a float for the decimal
+
+
+#q2
+# cats = input("how many cats do you have?")
+# dogs = input("how many dogs do you have?")
+# total = float(cats)*int(dogs)
+# print(f"Wow, if you multiplied them you would have {total} cats and dogs")
+
+
+#q3
+km = input("how many kilometers did you run?")
+m = int(float(km)*1000) 
+cm = int(float(km)*100000) 
+print (f"Wow, you ran {m}m or {cm}cm!")
+
+
+# km = input("how many kilometers did you run?")
+# m = float(km)*1000
+# cm = float(km)*100000
+# print (f"Wow, you ran {m}m or {cm}cm!")
+
+
+# km = float(input("how many kilometers did you run?"))
+# m = int(km)*1000
+# cm = int(km)*100000
+# print (f"Wow, you ran {m}m or {cm}cm!")
+
+
+#q4
+
+# name = input("Hi, whats your name?")
+# height = input("how tall are you?")
+
+# print(f"{name} is {height}cm tall")
